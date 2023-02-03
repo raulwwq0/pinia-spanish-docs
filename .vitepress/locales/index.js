b8bdc885d78fa1898f4c7172f8d5595d3f06fb94
Roger! <icarus.gk@gmail.com>
2023-02-03 12:15:22 -0600

    add: 'Idiomas' to navbar with zh and en

--- .vitepress/locales/index.js
+++ .vitepress/locales/index.js
@@ -1,10 +1,16 @@
 import es from './es.js'
+import en from './en.js'
+import zh from './zh.js'
 
 export default {
   vitepressConfig: {
-    '/': es.vitepressConfig,
+    '/en/': en.vitepressConfig, // It'll be '/' on prod
+    '/': es.vitepressConfig,    // It'll be '/es/' on prod
+    '/zh/': zh.vitepressConfig,
   },
   themeConfig: {
-    '/': es.themeConfig,
+    '/en/': en.themeConfig,     // It'll be '/' on prod
+    '/': es.themeConfig,        // It'll be '/es/' on prod
+    '/zh/': zh.themeConfig
   },
 }

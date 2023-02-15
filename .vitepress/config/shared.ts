b4dd147bc9084d5a27dd52ed6d06be66d1242f31
Raúl Aguilar <ag21raul@gmail.com>
2023-02-15 18:32:12 +0100

    chore: algolia translations

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -1,73 +1,76 @@
-import { defineConfig, HeadConfig } from 'vitepress'
+import { defineConfig, HeadConfig } from "vitepress";
 
-export const META_IMAGE = 'https://pinia.vuejs.org/social.png'
+export const META_IMAGE = "https://pinia.vuejs.org/social.png";
 export const isProduction =
-  process.env.NETLIFY && process.env.CONTEXT === 'production'
+    process.env.NETLIFY && process.env.CONTEXT === "production";
 
 if (process.env.NETLIFY) {
-  console.log('Netlify build', process.env.CONTEXT)
+    console.log("Netlify build", process.env.CONTEXT);
 }
 
 const productionHead: HeadConfig[] = [
-  [
-    'script',
-    {
-      src: 'https://unpkg.com/thesemetrics@latest',
-      async: '',
-      type: 'text/javascript',
-    },
-  ],
-]
+    [
+        "script",
+        {
+            src: "https://unpkg.com/thesemetrics@latest",
+            async: "",
+            type: "text/javascript",
+        },
+    ],
+];
 
 export const sharedConfig = defineConfig({
-  title: 'Pinia',
-  appearance: 'dark',
+    title: "Pinia",
+    appearance: "dark",
 
-  markdown: {
-    theme: {
-      dark: 'dracula-soft',
-      light: 'vitesse-light',
+    markdown: {
+        theme: {
+            dark: "dracula-soft",
+            light: "vitesse-light",
+        },
+
+        attrs: {
+            leftDelimiter: "%{",
+            rightDelimiter: "}%",
+        },
     },
 
-    attrs: {
-      leftDelimiter: '%{',
-      rightDelimiter: '}%',
-    },
-  },
+    head: [
+        ["link", { rel: "icon", type: "image/svg+xml", href: "/logo.svg" }],
+        ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
 
-  head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
-    ['link', { rel: 'icon', type: 'image/png', href: '/logo.png' }],
+        [
+            "meta",
+            {
+                name: "wwads-cn-verify",
+                content: "5878a7ab84fb43402106c575658472fa",
+            },
+        ],
 
-    [
-      'meta',
-      { name: 'wwads-cn-verify', content: '5878a7ab84fb43402106c575658472fa' },
-    ],
+        [
+            "meta",
+            {
+                property: "og:type",
+                content: "website",
+            },
+        ],
 
-    [
-      'meta',
-      {
-        property: 'og:type',
-        content: 'website',
-      },
-    ],
+        [
+            "meta",
+            {
+                property: "twitter:card",
+                content: "summary_large_image",
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "twitter:image",
+                content: META_IMAGE,
+            },
+        ],
 
-    [
-      'meta',
-      {
-        property: 'twitter:card',
-        content: 'summary_large_image',
-      },
-    ],
-    [
-      'meta',
-      {
-        property: 'twitter:image',
-        content: META_IMAGE,
-      },
-    ],
-
-    /* TODO: Remove comment when official docs fix the issue
+        /* TODO: Remove comment when official docs fix the issue
     [
       'script',
       {
@@ -78,45 +81,85 @@ export const sharedConfig = defineConfig({
       },
     ], */
 
-    ...(isProduction ? productionHead : []),
-  ],
-
-  themeConfig: {
-    logo: '/logo.svg',
-    outline: [2, 3],
-
-    socialLinks: [
-      { icon: 'twitter', link: 'https://twitter.com/posva' },
-      {
-        icon: 'github',
-        link: 'https://github.com/vuejs/pinia',
-      },
-      {
-        icon: 'discord',
-        link: 'https://chat.vuejs.org',
-      },
+        ...(isProduction ? productionHead : []),
     ],
 
-    footer: {
-      copyright: 'Copyright © 2019-present Eduardo San Martin Morote',
-      message: 'Lanzado bajo la Licencia MIT.',
-    },
+    themeConfig: {
+        logo: "/logo.svg",
+        outline: [2, 3],
 
-    editLink: {
-      pattern: 'https://github.com/vuejs/pinia/edit/v2/packages/docs/:path',
-      text: 'Sugerir cambios',
-    },
+        socialLinks: [
+            { icon: "twitter", link: "https://twitter.com/posva" },
+            {
+                icon: "github",
+                link: "https://github.com/vuejs/pinia",
+            },
+            {
+                icon: "discord",
+                link: "https://chat.vuejs.org",
+            },
+        ],
 
-    algolia: {
-      appId: 'WWHDVZOVW8',
-      apiKey: '9182688aa41745d0aa68ffa71f6b202b',
-      indexName: 'es-pinia',
-    },
+        footer: {
+            copyright: "Copyright © 2019-present Eduardo San Martin Morote",
+            message: "Lanzado bajo la Licencia MIT.",
+        },
 
-    carbonAds: {
-      code: 'CEBICK3I',
-      // custom: 'CEBICK3M',
-      placement: 'routervuejsorg',
+        editLink: {
+            pattern:
+                "https://github.com/vuejs/pinia/edit/v2/packages/docs/:path",
+            text: "Sugerir cambios",
+        },
+
+        algolia: {
+            appId: "WWHDVZOVW8",
+            apiKey: "9182688aa41745d0aa68ffa71f6b202b",
+            indexName: "es-pinia",
+            placeholder: "Buscar",
+            translations: {
+                button: {
+                    buttonText: "Buscar",
+                    buttonAriaLabel: "Buscar",
+                },
+                modal: {
+                    searchBox: {
+                        resetButtonTitle: "Borrar",
+                        resetButtonAriaLabel: "Borrar",
+                        cancelButtonText: "Cancelar",
+                        cancelButtonAriaLabel: "Cancelar",
+                    },
+                    startScreen: {
+                        recentSearchesTitle: "Recientes",
+                        noRecentSearchesText: "No hay busquedas recientes",
+                        saveRecentSearchButtonTitle: "Añadir a favoritos",
+                        removeRecentSearchButtonTitle: "Eliminar del historial de búsquedas",
+                        favoriteSearchesTitle: "Favoritos",
+                        removeFavoriteSearchButtonTitle: "Eliminar de favoritos",
+                    },
+                    errorScreen: {
+                        titleText: "No se ha encontrado",
+                        helpText: "Puede que necesites comprobar tu conexión a Internet",
+                    },
+                    footer: {
+                        selectText: "Seleccionar",
+                        navigateText: "Navegar",
+                        closeText: "Cerrar",
+                        searchByText: "Búsquedas por",
+                    },
+                    noResultsScreen: {
+                        noResultsText: "No se han encontrado resultados para",
+                        suggestedQueryText: "Quizás puedas encontrarlo en",
+                        reportMissingResultsText: "¿Crees que debería aparecer?",
+                        reportMissingResultsLinkText: "Haz clic aquí",
+                    },
+                },
+            },
+        },
+
+        carbonAds: {
+            code: "CEBICK3I",
+            // custom: 'CEBICK3M',
+            placement: "routervuejsorg",
+        },
     },
-  },
-})
+});

20785e66fc9af30c48058a2feb6099b6d5878ef0
Raúl Aguilar <ag21raul@gmail.com>
2023-07-22 20:46:50 +0200

    chore: vitepress up and add korean translation link (#99 and #100)

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vitepress'
-import { esConfig } from './es'
-import { sharedConfig } from './shared'
+import { defineConfig } from "vitepress";
+import { esConfig } from "./es";
+import { sharedConfig } from "./shared";
 
 export default defineConfig({
-  ...sharedConfig,
+    ...sharedConfig,
 
-  locales: {
-    root: { label: 'Español', lang: 'es', link: '/', ...esConfig },
-    en: { label: 'English', lang: 'en-US', link: 'https://pinia.vuejs.org/' },
-    zh: { label: '简体中文', lang: 'zh-CN', link: 'https://pinia.vuejs.org/zh/' },
-  },
-})
+    locales: {
+        root: { label: "Español", lang: "es", link: "/", ...esConfig },
+        en: {
+            label: "English",
+            lang: "en-US",
+            link: "https://pinia.vuejs.org/",
+        },
+        zh: {
+            label: "简体中文",
+            lang: "zh-CN",
+            link: "https://pinia.vuejs.org/zh/",
+        },
+        ko: {
+            label: "한국어",
+            lang: "ko-KR",
+            link: "https://pinia.vuejs.kr/",
+        },
+    },
+});
